edb1066524c32de4f6f1cb991d1fad7c5568135b
Kathleen E. Giebelhausen <KathleenEGiebelhausen@Kathleens-MacBook-Pro.local>
2018-04-24 16:31:31 -0700

    updated schema.sql to contain 10 items; finished the bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,10 +20,17 @@ connection.connect(function(err) {
 
 });
 function displayItems() {
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function(err, data) {
         if (err) throw err;
         // Log all results of the SELECT statement
-        console.log(res);
+        // console.log(data);
+        for(var i = 0; i < data.length; i++) {
+            console.log("ID: ", data[i].item_id)
+            console.log("Name: ", data[i].product_name)
+            console.log("Price: ", data[i].price) 
+            console.log("Quantity: ", data[i].stock_quantity);
+            console.log();
+        }
         promptCustomer();
       });
 }
@@ -43,13 +50,70 @@ function promptCustomer() {
     
     ]).then(answers => {
         // Use user feedback for... whatever!!
-        console.log(answers);
-        // "read? the database and check to see if the id exists
-        // if the id does not exist, and let the customer know and start them over again
-        // it it does exits, check to see if there is enough product
-        // if youd don't have enough, let the customer know and start them over again
-        // if you don have enough, let the customer know
-        // "update" the database
-        // and let customer know the total price
+        // console.log(answers);
+        // "read" the database and check to see if the id exists
+        // connection to the mysql database, and we query the database to see if product id exists
+        connection.query("SELECT * FROM products WHERE ?",
+        {
+          // this code fills in the question mark from line 49
+          // left side(item_id) = which came from the mysql database field name (the column name)
+          // right side (answers.id) = is the value we're looking for from that field
+          item_id: answers.id
+        },
+        // call back function happens when we get a response from the query on line 49
+        function(err, data) {
+            // this is just to see what data we got back from query (line 49) 
+            // console.log(data)
+            // if the id does not exist, then the date array will be empty 
+            if (data.length === 0){
+                //then we'll log a msg to the user
+                console.log("Sorry, product does not exist.")
+                // if the id does not exist, and let the customer know and start them over again
+                displayItems();
+            }
+            else {
+            // it it does exits, check to see if there is enough product
+              // making data[0] a variable of product so we can store the first object
+              // in the date array as product
+              var product = data[0];
+              //   console.log(product);
+            //   console.log("stock quantity:", product.stock_quantity);
+
+            // if you do have enough, let the customer know
+            // if the units ordered is < or = to quanity on hand
+              if (answers.units <= product.stock_quantity){
+                  // then it lets customer know the total price
+                  var totalPrice = answers.units * product.price;
+                  console.log("Your total price is $" + totalPrice);
+
+
+                  // and "updates" the database with the new quantity
+                  var updatedQuantity = product.stock_quantity - answers.units;
+                  connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                      {
+                        // this replaces the 1st question mark
+                        stock_quantity: updatedQuantity
+                      },
+                      {
+                        // this replaces the 2nd question mark
+                        // field name: value
+                        item_id: product.item_id
+                      }
+                    ],
+                    function(err, res) {
+                      console.log("Product purchased successfully.");
+                      connection.end();
+                    }
+                  );
+              } else {
+                  // if youd don't have enough, let the customer know and start them over again
+                  console.log("Sorry, we only have " + product.stock_quantity + " available.")
+                  displayItems();
+              }
+          }
+        }
+      );
     });
 };
